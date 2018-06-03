7837f8e23f7c48b29d45db902cf69156fc22c5e9
flar2 <asegaert@gmail.com>
2018-06-02 22:45:29 -0400

    drm-msm-dsi: add backlight min option
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: YousefAlgadri <yusufgadrie@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Signed-off-by: Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -34,6 +34,9 @@
 #include "dsi_pwr.h"
 #include "sde_dbg.h"
 
+static short backlight_min = 0;
+module_param(backlight_min, short, 0644);
+
 #define to_dsi_display(x) container_of(x, struct dsi_display, host)
 #define INT_BASE_10 10
 #define NO_OVERRIDE -1
@@ -178,6 +181,9 @@ int dsi_display_set_backlight(void *display, u32 bl_lvl)
 
 	bl_scale_ad = panel->bl_config.bl_scale_ad;
 	//bl_temp = (u32)bl_temp * bl_scale_ad / MAX_AD_BL_SCALE_LEVEL;
+
+	if (bl_temp != 0 && bl_temp < backlight_min)
+		bl_temp = backlight_min;
 
 	pr_debug("bl_scale = %u, bl_scale_ad = %u, bl_lvl = %u\n",
 		bl_scale, bl_scale_ad, (u32)bl_temp);
