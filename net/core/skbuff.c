736095fcf45cbe0b186c18670010622210a691f6
Ben Hutchings <ben@decadent.org.uk>
2018-08-26 22:47:27 +0100

    HACK: net: Avoid ABI change in 4.9.115
    Commit 2d08921c8da2 "tcp: free batches of packets in tcp_prune_ofo_queue()"
    moved sk_buff::pfmemalloc to a different place in the structure, so that
    __copy_skb_header() wouldn't copy it.
    
    To avoid an ABI change, move it back.  Change __copy_skb_header() to
    save and restore pfmemalloc so that it it not overwritten.
    
    [nc: Resolve conflicts and add HACK tag. A proper fix would just be to
         rebuild the Wi-Fi module against the new change but that isn't as
         easy as on regular Linux]
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: root <franciscofranco.1990@gmail.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -853,6 +853,8 @@ EXPORT_SYMBOL(napi_consume_skb);
 
 static void __copy_skb_header(struct sk_buff *new, const struct sk_buff *old)
 {
+	bool pfmemalloc;
+
 	new->tstamp		= old->tstamp;
 	/* We do not copy old->sk */
 	new->dev		= old->dev;
@@ -867,6 +869,8 @@ static void __copy_skb_header(struct sk_buff *new, const struct sk_buff *old)
 	 * It is not yet because we do not want to have a 16 bit hole
 	 */
 	new->queue_mapping = old->queue_mapping;
+
+	pfmemalloc = new->pfmemalloc;
 
 	memcpy(&new->headers_start, &old->headers_start,
 	       offsetof(struct sk_buff, headers_end) -
@@ -902,6 +906,7 @@ static void __copy_skb_header(struct sk_buff *new, const struct sk_buff *old)
 #endif
 #endif
 
+	new->pfmemalloc = pfmemalloc;
 }
 
 /*
