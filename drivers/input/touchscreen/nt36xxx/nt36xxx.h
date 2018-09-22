f601910108564e2a806a0fa3a32452e1fb68e48a
TheScarastic <warabhishek@gmail.com>
2018-09-22 05:17:00 -0400

    touchscreen: nt36xxx: Add DT2W switch and symlink it
    Change-Id: Iedf1c62a2f1f896a8d142deed69a6430623ae23f

--- drivers/input/touchscreen/nt36xxx/nt36xxx.h
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.h
@@ -157,6 +157,7 @@ struct nvt_ts_data {
 	struct work_struct resume_work;
 	struct workqueue_struct *event_wq;
 	struct completion dev_pm_suspend_completion;
+	struct proc_dir_entry *input_proc;
 };
 
 struct nvt_mode_switch {
