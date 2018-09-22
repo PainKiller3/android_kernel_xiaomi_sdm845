415c5e8b4766f689458e5859f3a48ec589329e57
TheScarastic <warabhishek@gmail.com>
2018-09-22 05:17:00 -0400

    touchscreen: nt36xxx: Add DT2W switch and symlink it
    Change-Id: Iedf1c62a2f1f896a8d142deed69a6430623ae23f
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- drivers/input/touchscreen/nt36xxx/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.c
@@ -67,6 +67,8 @@ extern void Boot_Update_Firmware(struct work_struct *work);
 static int nvt_drm_notifier_callback(struct notifier_block *self, unsigned long event, void *data);
 #endif
 
+#define PROC_SYMLINK_PATH "touchpanel"
+
 #if TOUCH_KEY_NUM > 0
 const uint16_t touch_key_array[TOUCH_KEY_NUM] = {
 	KEY_BACK,
@@ -1512,6 +1514,62 @@ err_pinctrl_get:
 	return retval;
 }
 
+static ssize_t nvt_panel_wake_gesture_show(struct device *dev,
+				     struct device_attribute *attr, char *buf)
+{
+        const char c = ts->gesture_enabled ? '1' : '0';
+        return sprintf(buf, "%c\n", c);
+}
+
+static ssize_t nvt_panel_wake_gesture_store(struct device *dev,
+				     struct device_attribute *attr, const char *buf, size_t count)
+{
+	int i;
+
+	if (sscanf(buf, "%u", &i) == 1 && i < 2) {
+		ts->gesture_enabled = i;
+		return count;
+	} else {
+		dev_dbg(dev, "enable_dt2w write error\n");
+		return -EINVAL;
+	}
+}
+
+static DEVICE_ATTR(wake_gesture, S_IWUSR | S_IRUSR,
+		nvt_panel_wake_gesture_show, nvt_panel_wake_gesture_store);
+
+static struct attribute *nvt_attr_group[] = {
+	&dev_attr_wake_gesture.attr,
+	NULL,
+};
+
+static ssize_t novatek_input_symlink(struct nvt_ts_data *ts) {
+	char *driver_path;
+	int ret = 0;
+	if (ts->input_proc) {
+		proc_remove(ts->input_proc);
+		ts->input_proc = NULL;
+	}
+	driver_path = kzalloc(PATH_MAX, GFP_KERNEL);
+	if (!driver_path) {
+		pr_err("%s: failed to allocate memory\n", __func__);
+		return -ENOMEM;
+	}
+
+	sprintf(driver_path, "/sys%s",
+			kobject_get_path(&ts->client->dev.kobj, GFP_KERNEL));
+
+	pr_err("%s: driver_path=%s\n", __func__, driver_path);
+
+	ts->input_proc = proc_symlink(PROC_SYMLINK_PATH, NULL, driver_path);
+
+	if (!ts->input_proc) {
+		ret = -ENOMEM;
+	}
+	kfree(driver_path);
+	return ret;
+}
+
 /*******************************************************
 Description:
 	Novatek touchscreen driver probe function.
@@ -1535,6 +1593,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	}
 
 	ts->client = client;
+	ts->input_proc = NULL; 
 	i2c_set_clientdata(client, ts);
 
 	//---parse dts---
@@ -1749,6 +1808,16 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 	}
 #endif
 
+	ts->attrs.attrs = nvt_attr_group;
+	ret = sysfs_create_group(&client->dev.kobj, &ts->attrs);
+	if (ret) {
+		NVT_ERR("Cannot create sysfs structure!\n");
+	} 
+	ret = novatek_input_symlink(ts);
+	if (ret < 0) {
+		NVT_ERR("Failed to symlink input device!\n");
+	}
+ 
 	bTouchIsAwake = 1;
 	NVT_LOG("end\n");
 
