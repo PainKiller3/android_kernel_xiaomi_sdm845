3b3a4ac3683d56031ea7bb67fd262a9bb330b74c
David Dai <daidavid1@codeaurora.org>
2018-12-11 18:52:56 -0800

    clk: qcom: clk-cpu-osm: move set policy->cpu flag to end of init
    In order to circumvent a race condition where one CPU may access the
    frequency table of the policy of another CPU while it's hotplugging,
    move setting of the policy->cpu flag to the end of the init
    osm operator after it initializes its frequency table.
    
    Change-Id: I4b02caa4995387c16a6db8b8eb9de7271f23c2f6
    Signed-off-by: David Dai <daidavid1@codeaurora.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- drivers/clk/qcom/clk-cpu-osm.c
+++ drivers/clk/qcom/clk-cpu-osm.c
@@ -97,6 +97,7 @@ struct clk_osm {
 	u32 prev_cycle_counter;
 	u32 max_core_count;
 	u32 mx_turbo_freq;
+	cpumask_t related_cpus;
 };
 
 static bool is_sdm845v1;
@@ -681,7 +682,7 @@ static struct clk_osm *osm_configure_policy(struct cpufreq_policy *policy)
 		if (parent != c_parent)
 			continue;
 
-		cpumask_set_cpu(cpu, policy->cpus);
+		cpumask_set_cpu(cpu, &c->related_cpus);
 		if (n->core_num == 0)
 			first = n;
 	}
@@ -815,6 +816,9 @@ static int osm_cpufreq_cpu_init(struct cpufreq_policy *policy)
 	}
 
 	policy->driver_data = c;
+
+	cpumask_copy(policy->cpus, &c->related_cpus);
+
 	return 0;
 
 err:
