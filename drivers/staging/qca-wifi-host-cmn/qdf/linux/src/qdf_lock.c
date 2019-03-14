4717ab9ae6740bb6bf40149a45f246f6b313d795
Tyler Nijmeh <tylernij@gmail.com>
2019-03-13 20:21:57 -0700

    qcacld-3.0: Do not allow any wakelocks to be held
    These contribute to a great amount of idle drain.
    
    Tests: 30 minutes of playing Spotify with the screen off, unplugged.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    
     Conflicts:
    	drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_lock.c
    
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_lock.c
+++ drivers/staging/qca-wifi-host-cmn/qdf/linux/src/qdf_lock.c
@@ -241,19 +241,10 @@ qdf_export_symbol(qdf_mutex_release);
  *
  * Return: Pointer to the name if it is valid or a default string
  */
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0))
-const char *qdf_wake_lock_name(qdf_wake_lock_t *lock)
-{
-	if (lock)
-		return lock->lock.name;
-	return "UNNAMED_WAKELOCK";
-}
-#else
 const char *qdf_wake_lock_name(qdf_wake_lock_t *lock)
 {
 	return "NO_WAKELOCK_SUPPORT";
 }
-#endif
 qdf_export_symbol(qdf_wake_lock_name);
 
 /**
@@ -304,22 +295,10 @@ qdf_export_symbol(qdf_wake_lock_create);
  * QDF status success: if wake lock is acquired
  * QDF status failure: if wake lock was not acquired
  */
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0))
-QDF_STATUS qdf_wake_lock_acquire(qdf_wake_lock_t *lock, uint32_t reason)
-{
-	host_diag_log_wlock(reason, qdf_wake_lock_name(lock),
-			    WIFI_POWER_EVENT_DEFAULT_WAKELOCK_TIMEOUT,
-			    WIFI_POWER_EVENT_WAKELOCK_TAKEN);
-	__pm_stay_awake(lock->priv);
-
-	return QDF_STATUS_SUCCESS;
-}
-#else
 QDF_STATUS qdf_wake_lock_acquire(qdf_wake_lock_t *lock, uint32_t reason)
 {
 	return QDF_STATUS_SUCCESS;
 }
-#endif
 qdf_export_symbol(qdf_wake_lock_acquire);
 
 /**
@@ -331,27 +310,10 @@ qdf_export_symbol(qdf_wake_lock_acquire);
  * QDF status success: if wake lock is acquired
  * QDF status failure: if wake lock was not acquired
  */
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 12, 0)
-QDF_STATUS qdf_wake_lock_timeout_acquire(qdf_wake_lock_t *lock, uint32_t msec)
-{
-	pm_wakeup_ws_event(lock->priv, msec, true);
-	return QDF_STATUS_SUCCESS;
-}
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0)
-QDF_STATUS qdf_wake_lock_timeout_acquire(qdf_wake_lock_t *lock, uint32_t msec)
-{
-	/* Wakelock for Rx is frequent.
-	 * It is reported only during active debug
-	 */
-	__pm_wakeup_event(&(lock->lock), msec);
-	return QDF_STATUS_SUCCESS;
-}
-#else /* LINUX_VERSION_CODE */
 QDF_STATUS qdf_wake_lock_timeout_acquire(qdf_wake_lock_t *lock, uint32_t msec)
 {
 	return QDF_STATUS_SUCCESS;
 }
-#endif /* LINUX_VERSION_CODE */
 qdf_export_symbol(qdf_wake_lock_timeout_acquire);
 
 /**
@@ -363,22 +325,10 @@ qdf_export_symbol(qdf_wake_lock_timeout_acquire);
  * QDF status success: if wake lock is acquired
  * QDF status failure: if wake lock was not acquired
  */
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0))
-QDF_STATUS qdf_wake_lock_release(qdf_wake_lock_t *lock, uint32_t reason)
-{
-	host_diag_log_wlock(reason, qdf_wake_lock_name(lock),
-			    WIFI_POWER_EVENT_DEFAULT_WAKELOCK_TIMEOUT,
-			    WIFI_POWER_EVENT_WAKELOCK_RELEASED);
-	__pm_relax(lock->priv);
-
-	return QDF_STATUS_SUCCESS;
-}
-#else
 QDF_STATUS qdf_wake_lock_release(qdf_wake_lock_t *lock, uint32_t reason)
 {
 	return QDF_STATUS_SUCCESS;
 }
-#endif
 qdf_export_symbol(qdf_wake_lock_release);
 
 /**
