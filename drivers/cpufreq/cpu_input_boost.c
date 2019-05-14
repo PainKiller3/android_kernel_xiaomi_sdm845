9c7c8021687333c19a7b06334508788ffa38375c
0ctobot <return.of.octobot@gmail.com>
2019-05-13 22:03:09 -0400

    cpu_input_boost: Allow configuration of LP cluster's minimum idle frequency
    Subsequent to:
    447dc35 ("cpu_input_boost: Remove frame boosting entirely [1/2]")
    6838cc8 ("drm/atomic: Remove frame boosting entirely [2/2]")
    
    In lieu of maintaining a seperate boost level for rendering, we
    will be globally raising the active minimum frequency for LP
    cluster to 748MHz. In order to mitigate any negative impact this
    may have on idle drain, allow for secondary configuration of LP's
    screen-off minimum.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: Raphiel Rollerscaperers <raphielscape@outlook.com>
    Signed-off-by: Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -27,6 +27,8 @@ static unsigned int max_boost_freq_lp __read_mostly =
 	CONFIG_MAX_BOOST_FREQ_LP;
 static unsigned int max_boost_freq_hp __read_mostly =
 	CONFIG_MAX_BOOST_FREQ_PERF;
+static unsigned int idle_min_freq_lp __read_mostly =
+	CONFIG_IDLE_MIN_FREQ_LP;
 static unsigned int remove_input_boost_freq_lp __read_mostly =
 	CONFIG_REMOVE_INPUT_BOOST_FREQ_LP;
 static unsigned int remove_input_boost_freq_perf __read_mostly =
@@ -41,6 +43,7 @@ module_param(input_boost_freq_lp, uint, 0644);
 module_param(input_boost_freq_hp, uint, 0644);
 module_param(max_boost_freq_lp, uint, 0644);
 module_param(max_boost_freq_hp, uint, 0644);
+module_param(idle_min_freq_lp, uint, 0644);
 module_param(remove_input_boost_freq_lp, uint, 0644);
 module_param(remove_input_boost_freq_perf, uint, 0644);
 
@@ -109,9 +112,13 @@ static unsigned int get_max_boost_freq(struct cpufreq_policy *policy)
 
 static unsigned int get_min_freq(struct cpufreq_policy *policy)
 {
+	struct boost_drv *b = &boost_drv_g;
 	unsigned int freq;
 
-	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
+	if (cpumask_test_cpu(policy->cpu, cpu_lp_mask) &&
+			test_bit(SCREEN_OFF, &b->state))
+		freq = idle_min_freq_lp;
+	else if (cpumask_test_cpu(policy->cpu, cpu_lp_mask))
 		freq = remove_input_boost_freq_lp;
 	else
 		freq = remove_input_boost_freq_perf;
