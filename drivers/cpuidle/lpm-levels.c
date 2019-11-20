ac5f1a964005b35007a14b1f9f55bdde591f1092
Jia-yi Chen <jychen@google.com>
2019-11-20 11:18:28 -0800

    cpuidle: Change idle policy to always enter C1 first
    Change the cpuidle policy to always enter C1 first and set a timer
    with a duration of half the C1_max_residency. Once the timer expired,
    it can then enter C4.
    
    Bug: 143480592
    Test: Verify idle behavior and check power & perf on several use cases.
    Change-Id: I3c54e34b7af2f262bd029f48fef4e00620536586
    Signed-off-by: Jia-yi Chen <jychen@google.com>
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -412,7 +412,7 @@ static uint64_t lpm_cpuidle_predict(struct cpuidle_device *dev,
 		history->hinvalid = 0;
 		history->htmr_wkup = 1;
 		history->stime = 0;
-		return 0;
+		return 1;
 	}
 
 	/*
@@ -615,8 +615,10 @@ static int cpu_power_select(struct cpuidle_device *dev,
 			 * call prediction.
 			 */
 			if (next_wakeup_us > max_residency[i]) {
-				predicted = lpm_cpuidle_predict(dev, cpu,
-					&idx_restrict, &idx_restrict_time);
+				predicted = (lpm_cpuidle_predict(dev, cpu,
+					&idx_restrict,
+					&idx_restrict_time) == 1) ? 0 :
+						(max_residency[i] >> 1);
 				if (predicted && (predicted < min_residency[i]))
 					predicted = min_residency[i];
 			} else
