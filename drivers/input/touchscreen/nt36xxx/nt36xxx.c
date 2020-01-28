cd71f397c02564756e64a08d00859787782850f3
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-28 14:11:48 +0900

    nt36xxx: remove IRQ toggling under IRQ handler
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- drivers/input/touchscreen/nt36xxx/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.c
@@ -1174,7 +1174,6 @@ XFER_ERROR:
 	input_sync(ts->input_dev);
 
 out:
-	enable_irq(ts->client->irq);
 	mutex_unlock(&ts->lock);
 }
 
@@ -1187,7 +1186,6 @@ return:
 *******************************************************/
 static irqreturn_t nvt_ts_irq_handler(int32_t irq, void *dev_id)
 {
-	disable_irq_nosync(ts->client->irq);
 	if (bTouchIsAwake == 0) {
 		dev_dbg(&ts->client->dev, "%s gesture wakeup\n", __func__);
 	}
