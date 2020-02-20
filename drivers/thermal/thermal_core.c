0994d8bbcb5343c2355d3c9b2eeb3f6f0c3eb363
Pierre2324 <pdbbasketball@gmail.com>
2020-02-19 19:29:27 -0500

    thermal: Implement a thermal locking mecanism
    Some ROMs (mainly MIUI) can override user/kernel specific thermal selection
    so add a small thermal feature to lock a specific selection. When enabled,
    kernel will use and lock specified thermal selection. When disabled, kernel
    will follow default ROM/kernel behavior.
    
    Note: this is used by my NoGravity Kernel App to lock evaluation (NGK thermals)
    while leaving the choice to user to run default ROM configuration or own selection
    when not on a dynamic thermal ROM like MIUI.
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -90,7 +90,11 @@ static char boost_buf[128];
 #ifdef CONFIG_THERMAL_SUSPEND_RESUME
 static int prev_sconfig = 10;
 static int suspend_sconfig = -1;
+static int lock_enable = 0;
+static int lock_sconfig = -1;
 module_param(suspend_sconfig, int, 0644);
+module_param(lock_enable, int, 0644);
+module_param(lock_sconfig, int, 0644);
 #endif
 
 static struct thermal_governor *__find_governor(const char *name)
@@ -2707,7 +2711,10 @@ thermal_sconfig_store(struct device *dev,
 	int val = -1;
 
 	rv = kstrtoint(buf, 10, &val);
-	atomic_set(&switch_mode, val);
+	if(lock_enable)
+		atomic_set(&switch_mode, lock_sconfig);
+	else
+		atomic_set(&switch_mode, val);
 
 	if (rv)
 		return rv;
