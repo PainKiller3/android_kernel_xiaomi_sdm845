2828707d1e5262930c66dc224ea868ef99e3ab35
Pierre2324 <pdbbasketball@gmail.com>
2020-02-26 13:47:32 -0500

    cpufreq: Make userspace CPU boost optional
    Some ROMs use weird userspace boosting that overrides kernel configuration.
    Userspace boosting has been disabled to give kernel full control, but in some
    case we may want to be able to enable it back so make it optional.
    (Mostly for testing purpose)
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmaill.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -34,8 +34,12 @@
 #include <linux/sched.h>
 #endif
 #include <trace/events/power.h>
+#include <linux/moduleparam.h>
 
 static LIST_HEAD(cpufreq_policy_list);
+
+static int userspace_boost_enable = 0;
+module_param(userspace_boost_enable, int, 0644);
 
 static inline bool policy_is_inactive(struct cpufreq_policy *policy)
 {
@@ -772,8 +776,9 @@ static ssize_t store_##file_name					\
 	int ret, temp;							\
 	struct cpufreq_policy new_policy;				\
 									\
-	if (&policy->object == &policy->min)				\
-		return count;						\
+	if(!userspace_boost_enable)				\
+		if (&policy->object == &policy->min)				\
+			return count;						\
 									\
 	memcpy(&new_policy, policy, sizeof(*policy));			\
 	new_policy.min = policy->user_policy.min;			\
