56a6627662e7ace3975ba9a8d8ec01f41ca2e3a5
Rapherion Rollerscaperers <rapherion@raphielgang.org>
2020-03-01 03:54:27 +0700

    qpnp-fg-gen3: add delay for soc_work
    Getting soc_work need a bit of time
    
    Delay it to 1 second because yes.
    
    Signed-off-by: Rapherion Rollerscaperers <rapherion@raphielgang.org>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -6052,6 +6052,7 @@ static struct thermal_zone_of_device_ops fg_gen3_tz_ops = {
 };
 
 #define FG_DELAY_BATT_ID_MS 1000
+#define FG_DELAY_SOC_WORK_MS 1000
 static int fg_gen3_probe(struct platform_device *pdev)
 {
 	struct fg_chip *chip;
@@ -6275,7 +6276,8 @@ static int fg_gen3_probe(struct platform_device *pdev)
 	device_init_wakeup(chip->dev, true);
 	schedule_delayed_work(&chip->profile_load_work,
 			      msecs_to_jiffies(FG_DELAY_BATT_ID_MS));
-	schedule_delayed_work(&chip->soc_work, 0);
+	schedule_delayed_work(&chip->soc_work,
+			      msecs_to_jiffies(FG_DELAY_SOC_WORK_MS));
 
 	chip->param.batt_soc = -EINVAL;
 	schedule_delayed_work(&chip->soc_monitor_work,
