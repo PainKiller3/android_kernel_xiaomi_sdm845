e1baea73009665e75a8a14afbbe4b572adcf3e84
Khusika Dhamar Gusti <mail@khusika.com>
2020-06-02 20:54:28 +0700

    usb: gadget: Update default interface descriptor class
    Xiaoi also did this changes in this RNDIS class, required after this change
    ee29216 "usb: gadget: Update default RNDIS class for Xiaomi".
    
    Change-Id: I217e005fe37aa6f5dffb535122b44c17c56235c7

--- drivers/usb/gadget/function/f_qc_rndis.c
+++ drivers/usb/gadget/function/f_qc_rndis.c
@@ -224,9 +224,9 @@ rndis_qc_iad_descriptor = {
 	.bDescriptorType =	USB_DT_INTERFACE_ASSOCIATION,
 	.bFirstInterface =	0, /* XXX, hardcoded */
 	.bInterfaceCount =	2, /* control + data */
-	.bFunctionClass =	USB_CLASS_WIRELESS_CONTROLLER,
-	.bFunctionSubClass =	0x01,
-	.bFunctionProtocol =	0x03,
+	.bFunctionClass =	USB_CLASS_MISC,
+	.bFunctionSubClass =	0x04,
+	.bFunctionProtocol =	0x01,
 	/* .iFunction = DYNAMIC */
 };
 
