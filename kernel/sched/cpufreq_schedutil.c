10a9e363cc6ae358efda724f3ad01f296283b18a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-09-07 18:37:36 +0530

    cpufreq: schedutil: Disable iowait boost with battery saver
    Change-Id: I3c595c8c2c3c70f486a03119ab127bb3e3da6c0a
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -16,6 +16,7 @@
 #include <linux/slab.h>
 #include <trace/events/power.h>
 #include <linux/sched/sysctl.h>
+#include <linux/battery_saver.h>
 #include "sched.h"
 #include "tune.h"
 
@@ -219,6 +220,9 @@ static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu)
 static void sugov_set_iowait_boost(struct sugov_cpu *sg_cpu, u64 time,
 				   unsigned int flags)
 {
+	if (is_battery_saver_on())
+		return;
+
 	if (flags & SCHED_CPUFREQ_IOWAIT) {
 		sg_cpu->iowait_boost = sg_cpu->iowait_boost_max;
 	} else if (sg_cpu->iowait_boost) {
