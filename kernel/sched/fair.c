038b4e756ad8d0bafaf83d1eef162f8e0543faf5
darkhz <kmachanwenw@gmail.com>
2021-01-29 11:35:37 +0530

    sched/fair: Make boosted and prefer_idle tunables uclamp aware
    Since we now have uclamp_boosted() and uclamp_latency_sensitive(),
    which is similar to schedtune_task_boost() and schedtune_prefer_idle()
    respectively, use them.
    
    Signed-off-by: CloudedQuartz <ravenklawasd@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7523,6 +7523,9 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu)
 #ifdef CONFIG_CGROUP_SCHEDTUNE
 	boosted = schedtune_task_boost(p) > 0;
 	prefer_idle = schedtune_prefer_idle(p) > 0;
+#elif  CONFIG_UCLAMP_TASK
+	boosted = uclamp_boosted(p);
+	prefer_idle = uclamp_latency_sensitive(p);
 #else
 	boosted = get_sysctl_sched_cfs_boost() > 0;
 	prefer_idle = 0;
