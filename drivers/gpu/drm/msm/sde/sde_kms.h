94b939a6e8aa45bc6295840b8a641d7bb589737d
Sultan Alsawaf <sultan@kerneltoast.com>
2021-02-19 22:47:37 -0800

    disp: msm: sde: Stub out debug log macros and compile them out
    These debug logs are everywhere and not only bloat the driver, but add
    latency everywhere they're used because they're not compiled out. Since
    they serve no purpose for us as we're not debugging SDE, compile them
    out.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: atndko <z1281552865@gmail.com>
    Change-Id: I7a39b33ab7bedcd6fc4633eac231a7d12fe7642c
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_kms.h
+++ drivers/gpu/drm/msm/sde/sde_kms.h
@@ -47,24 +47,14 @@
  * @fmt: Pointer to format string
  */
 #define SDE_DEBUG(fmt, ...)                                                \
-	do {                                                               \
-		if (unlikely(drm_debug & DRM_UT_KMS))                      \
-			DRM_DEBUG(fmt, ##__VA_ARGS__); \
-		else                                                       \
-			pr_debug(fmt, ##__VA_ARGS__);                      \
-	} while (0)
+	no_printk(fmt, ##__VA_ARGS__)
 
 /**
  * SDE_DEBUG_DRIVER - macro for hardware driver logging
  * @fmt: Pointer to format string
  */
 #define SDE_DEBUG_DRIVER(fmt, ...)                                         \
-	do {                                                               \
-		if (unlikely(drm_debug & DRM_UT_DRIVER))                   \
-			DRM_ERROR(fmt, ##__VA_ARGS__); \
-		else                                                       \
-			pr_debug(fmt, ##__VA_ARGS__);                      \
-	} while (0)
+	no_printk(fmt, ##__VA_ARGS__)
 
 #define SDE_ERROR(fmt, ...) pr_err("[sde error]" fmt, ##__VA_ARGS__)
 
