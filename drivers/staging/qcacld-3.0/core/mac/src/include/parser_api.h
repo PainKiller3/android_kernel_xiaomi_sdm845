4821c8ca4255a8ebdd4270ba94c60dd9047cc24b
CloudedQuartz <ravenklawasd@gmail.com>
2021-05-20 10:04:21 +0200

    qcacld-3.0: Fix multiple warnings

--- drivers/staging/qcacld-3.0/core/mac/src/include/parser_api.h
+++ drivers/staging/qcacld-3.0/core/mac/src/include/parser_api.h
@@ -1121,7 +1121,7 @@ QDF_STATUS populate_dot11f_rrm_ie(tpAniSirGlobal pMac,
 				tpPESession psessionEntry);
 
 void populate_mdie(tpAniSirGlobal pMac, tDot11fIEMobilityDomain * pDot11f,
-		   uint8_t mdie[]);
+		   uint8_t mdie[SIR_MDIE_SIZE]);
 
 #ifdef WLAN_FEATURE_FILS_SK
 /**
